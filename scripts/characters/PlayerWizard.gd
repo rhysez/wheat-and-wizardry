10e9890623f2fd4bbde606c8a1976232911b8d30
Rhys Hodgson <121772484+rhysez@users.noreply.github.com>
2024-08-18 20:37:59 +0100

    import run animation and temporary grass texture

--- scripts/characters/PlayerWizard.gd
+++ scripts/characters/PlayerWizard.gd
@@ -2,8 +2,17 @@ extends CharacterBody2D
 
 const MOVE_SPEED = 180
 
+@onready var animated_sprite = $AnimatedSprite2D
+
 func _physics_process(delta):
 	var direction = Input.get_vector("move_left", "move_right", "move_up", "move_down")
 	velocity = direction * MOVE_SPEED
+	
+	print(typeof(direction))
+	
+	if direction.length() > 0:
+		animated_sprite.play("run")
+	else:
+		animated_sprite.play("default")
 	
 	move_and_slide()

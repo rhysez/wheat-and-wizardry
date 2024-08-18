3f12305085a6331ff7b47838f2e9782bf4a9a773
Rhys Hodgson <121772484+rhysez@users.noreply.github.com>
2024-08-18 19:38:08 +0100

    reduce move speed on player and projectiles, add camera smoothing

--- scripts/characters/PlayerWizard.gd
+++ scripts/characters/PlayerWizard.gd
@@ -1,6 +1,6 @@
 extends CharacterBody2D
 
-const MOVE_SPEED = 600
+const MOVE_SPEED = 180
 
 func _physics_process(delta):
 	var direction = Input.get_vector("move_left", "move_right", "move_up", "move_down")
